19275f94b8a41998678ed280ffd6b9412356a7cc
yellowstoneparks <otzarj@nycstudents.net>
2024-11-26 15:30:34 -0500

    main.c working

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/wait.h>
 
 int main() {
 	printf("%d about to create 2 child processes\n", getpid());
+	int ppid = getpid();
 
 	pid_t p_1; pid_t p_2;
 	p_1 = fork();
@@ -12,21 +15,42 @@ int main() {
 		perror("fork fail");
 		exit(1);
 	} else if (p_1 == 0) {
-		// child1
-		printf("child 1\n");
-
 		p_2 = fork();
 		if (p_2 < 0) {
 			perror("fork fail");
 		} else if (p_2 == 0) {
 			// child2
-			printf("child 2\n");
+			int pid = getpid();
+			srand(pid);
+			int sec = rand() % 5 + 1;
+			printf("%d %dsec\n", pid, sec);
+			sleep(sec);
+			printf("%d finished after %d sec\n", pid, sec);
+			exit(sec);
 		} else {
-			// back to child 1
-			exit(1);
+			// child 1
+			int pid = getpid();
+			srand(pid);
+			int sec = rand() % 5 + 1;
+			printf("%d %dsec\n", pid, sec);
+			sleep(sec);
+			printf("%d finished after %d sec\n", pid, sec);
+			exit(sec);
 		}
 	} else {
 		// parent
-		printf("parent\n");
+		int status;
+		while (1) {
+			pid_t wpid = waitpid(p_1, &status, WNOHANG);
+
+			if (wpid == -1) {
+				exit(0);
+			}	else if (wpid == 0) {
+				sleep(1);
+			} else {
+				printf("Main process %d is done. Child %d slept for %d seconds\n", ppid, p_1, WEXITSTATUS(status));
+				break;
+			}
+		}	
 	}
 }
---
+++ makefile
@@ -0,0 +1,12 @@
+compile main: main.o
+	gcc -o main main.o
+
+main.o: main.c
+	gcc -c main.c
+
+run: main
+	./main
+
+clean:
+	rm -f *.o
+	rm -f main
